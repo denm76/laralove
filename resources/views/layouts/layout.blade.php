c60794c98eec54d32404599b58798baac6e15dc0
denm76 <72597356+denm76@users.noreply.github.com>
2020-12-01 12:19:35 +0200

    Установлена пагинация, добавлена страница  поста

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+    <div class="container collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="col-6 navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="/">Главная <span class="sr-only">(current)</span></a>
+            </li>
+
+            <li class="nav-item active offset-3">
+                <a class="nav-link" href="{{ route('post.create') }}">Создать пост <span class="sr-only">(current)</span></a>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0" action="{{route('post.index')}}">
+            <input class="form-control mr-sm-2" name="search" type="search" placeholder="Найти пост..." aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Поиск</button>
+        </form>
+    </div>
+</nav>
+
+@if(isset($_GET['search']))
+
+    @if(count($posts)>0)
+        <center>
+            <h2>Результаты поиска по запросу <?php $_GET['search'] ?></h2>
+            <p class="lead">Всего найдено {{count($posts)}} постов</p>
+        </center>
+    @else
+        <center>
+            <h2>По запросу <?php $_GET['search'] ?>&nbsp ничего не найдено</h2>
+            <a href="{{ route('post.index') }}" class="btn btn-outline-primary">Отобразить все посты</a>
+        </center>
+    @endif
+
+@endif
+
+<div class="container">
+    @yield('content');
+</div>
+</body>
+</html>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,63 +1,41 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
+@extends('layouts.layout')
+@section('content')
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    Dropdown
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-            </li>
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-    </div>
-</nav>
+    @if(isset($_GET['search']))
 
-<div class="container">
+        @if(count($posts)>0)
+            <center>
+            <h2>Результаты поиска по запросу <?php $_GET['search'] ?></h2>
+            <p class="lead">Всего найдено {{count($posts)}} постов</p>
+            </center>
+        @else
+            <center>
+            <h2>По запросу <?php $_GET['search'] ?>&nbsp ничего не найдено</h2>
+                <a href="{{ route('post.index') }}" class="btn btn-outline-primary">Отобразить все посты</a>
+            </center>
+        @endif
+
+    @endif
+
+
     <div class="row">
         @foreach($posts as $post)
-        <col-md-6>
-            <div class="card">
-                <div class="card-header">{{$post->short_title}}</div>
-                <div class="card-body">{{$post->descr}}</div>
-            </div>
-        </col-md-6>
+            <col-md-6>
+                <div class="card">
+                    <div class="card-header"><h2>{{$post->short_title}}</h2></div>
+                    <div class="card-body">
+                        <div class="card-img" style="background-image:url({{$post->img ?? asset('img/default.jpg')}})"></div>
+                        <div class="card-author">Автор:{{$post->name}}</div>
+                        <a href="" class="btn btn-outline-primary">Посмотреть пост</a>
+                    </div>
+                </div>
+            </col-md-6 >
         @endforeach
     </div>
-</div>
-</body>
-</html>
+
+    @if(!isset($_GET['search']))
+      {{ $posts->links() }}
+    @endif
+
+@endsection
 
