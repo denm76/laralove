1e441f4812fe51fbed54caf57c186eefbca132d5
denm76 <72597356+denm76@users.noreply.github.com>
2020-12-08 16:29:27 +0200

    Добавлены методы edit,show

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', 'PostController@index');
 Route::get('post/', 'PostController@index')->name('post.index');
 Route::get('post/create', 'PostController@create')->name('post.create');
+Route::get('post/show/{id}', 'PostController@show')->name('post.show');
+Route::get('post/edit/{id}', 'PostController@edit')->name('post.edit');
+Route::delete('post/{id}', 'PostController@destroy')->name('post.destroy');
+Route::patch('post/show/{id}', 'PostController@update')->name('post.update');
+Route::post('post/', 'PostController@store')->name('post.store');
